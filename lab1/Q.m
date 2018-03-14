d3a3b08d71d944ca601cd4b9182a41e0a897fa9c
mckib2 <nicholas.bgp@gmail.com>
2018-03-13 21:27:45 -0600

    Made sure Q worked the same as Moon's.

--- lab1/Q.m
+++ lab1/Q.m
@@ -1,11 +1,23 @@
 %% Helper Functions
-function [ out ] = Q(x)
-    out = zeros(1,numel(x));
-    for ii = 1:numel(x)
-       out(ii) = 1/sqrt(2*pi)*integral(@probfun,x(ii),Inf);
+% function [ out ] = Q(x)
+%     out = zeros(1,numel(x));
+%     for ii = 1:numel(x)
+%        out(ii) = 1/sqrt(2*pi)*integral(@probfun,x(ii),Inf);
+%     end
+% end
+% 
+% function [ out ] = probfun(n)
+%     out = exp(-n.^2/2);
+% end
+function [ p ] = Q(xlist)
+    p = zeros(size(xlist));
+    i = 0;
+    for x = xlist
+        i = i+1;
+        if(x < 0)
+            p(i) = 1- 0.5*erfc(-x/sqrt(2));
+        else
+            p(i) = 0.5*erfc(x/sqrt(2));
+        end
     end
-end
-
-function [ out ] = probfun(n)
-    out = exp(-n.^2/2);
 end
